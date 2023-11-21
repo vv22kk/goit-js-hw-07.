0d9196ee1a14ef333f0a435ebcdf81c6e8e9ee27
Виталий <89840918+vv22kk@users.noreply.github.com>
2023-11-21 23:57:09 +0200

    add:alt,esc,

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// ---create-galery
 const galleryEl = document.querySelector('.gallery');
 galleryEl.addEventListener('click', GaleryClick);
 
@@ -23,17 +24,19 @@ function createGalerry(arr) {
 }
 galleryEl.insertAdjacentHTML('beforeend', createGalerry(galleryItems));
 
+//create-modal-Original-Size (basicLightbox)
 function GaleryClick(event) {
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  const targetClickImageOriginalSize = event.target.dataset.source;
+
   const instance = basicLightbox.create(
-    `<img src = "${targetClickImageOriginalSize}">`,
+    `<img src = "${event.target.dataset.source}" alt='${event.target.alt}'>`,
   );
   instance.show();
 
+  // ---close-modal-ESC
   const closeModal = e => {
     if (e.key === 'Escape') {
       instance.close();
